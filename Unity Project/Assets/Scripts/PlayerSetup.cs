3f02b627da43c44d74c2a08777ece3c03d386cdf
BlackthornProd <blackthornprod@gmail.com>
2017-10-29 20:33:18 +0100

    Revert "Revert "Player movement & Weapon rotation - Multiplayer""
    This reverts commit c9621278352879b064e5e0a82d46ba1ed844d3a1.

---
+++ Unity Project/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	public Behaviour[] componentsToEnable;
+
+	void Start () {
+		if (isLocalPlayer) {
+			foreach (Behaviour component in componentsToEnable) {
+				component.enabled = true;
+			}
+		}
+	}
+
+}
