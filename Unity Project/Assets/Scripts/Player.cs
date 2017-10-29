c693dc4b7cd3fc2f4e1699f29d15c5af043ad88e
BlackthornProd <blackthornprod@gmail.com>
2017-10-29 16:45:02 +0100

    Player movement & Gun rotation

---
+++ Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour {
+
+	PlayerController controller;
+
+	public float speed = 7f;
+	public Transform weaponHolder;
+
+	void Start () {
+		controller = GetComponent <PlayerController> (); 
+	}
+
+	void Update () {
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		Vector2 velocity = input.normalized * speed;
+		controller.Move (velocity);
+
+		Vector3 difference = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - weaponHolder.position).normalized;
+		float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
+		weaponHolder.rotation = Quaternion.Euler (0f, 0f, rotZ - 90);
+
+	}
+
+
+}
---
+++ Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	Vector2 velocity;
+
+	void Start () {
+		rb = GetComponent <Rigidbody2D> ();
+	}
+
+	public void Move (Vector2 newVelocity) {
+		velocity = newVelocity;
+	}
+
+	void FixedUpdate () {
+		rb.position += velocity * Time.fixedDeltaTime;
+	}
+
+}
